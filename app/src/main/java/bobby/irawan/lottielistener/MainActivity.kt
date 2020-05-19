afba48574f9ef8e1fa5355aaefe2295f281a001b
Bobby irawan <irawanbobby09@gmail.com>
2020-05-19 10:07:20 +0700

    code improvement

--- app/src/main/java/bobby/irawan/lottielistener/MainActivity.kt
+++ app/src/main/java/bobby/irawan/lottielistener/MainActivity.kt
@@ -3,6 +3,7 @@ package bobby.irawan.lottielistener
 import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,32 +16,34 @@ class MainActivity : AppCompatActivity() {
         lottie_animation_view.setAnimationFromUrl("https://assets7.lottiefiles.com/private_files/lf30_yP8raD.json")
         lottie_animation_view.addAnimatorUpdateListener {
             val progress = (it.animatedValue as Float * 100).toInt()
-            text_view_progress.text = progress.toString()
-            if (progress >= 50 && progress != 0) {
-                lottie_animation_view.pauseAnimation()
-                text_view_information.text = "Already hit 50%"
-            } else {
-                text_view_information.text = ""
-            }
+            text_view_progress.text = progress.toString() + "%"
+//            if (progress >= 50 && progress != 0) {
+//                lottie_animation_view.pauseAnimation()
+//                text_view_information.text = "Already hit 50%"
+//            } else {
+//                text_view_information.text = ""
+//            }
+        }
+
+        button_cancel.setOnClickListener {
+            lottie_animation_view.cancelAnimation()
         }
 
         lottie_animation_view.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationRepeat(animation: Animator?) {
-                Toast.makeText(this@MainActivity, "Start over the animation", Toast.LENGTH_SHORT)
-                    .show()
+                //Do nothing
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                Toast.makeText(this@MainActivity, "Animation is end", Toast.LENGTH_SHORT).show()
+                //Do nothing
             }
 
             override fun onAnimationCancel(animation: Animator?) {
-                Toast.makeText(this@MainActivity, "Animation is cancelled", Toast.LENGTH_SHORT)
-                    .show()
+                lottie_animation_view.visibility = View.GONE
             }
 
             override fun onAnimationStart(animation: Animator?) {
-                Toast.makeText(this@MainActivity, "Start the animation", Toast.LENGTH_SHORT).show()
+                //Do nothing
             }
 
         })
